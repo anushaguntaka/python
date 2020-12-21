496d873924d6d0cf760cb58546946c0602786458
anushaguntaka <33531186+anushaguntaka@users.noreply.github.com>
2020-12-21 16:17:57 -0700

    Add files via upload
    #1 Write a Python program to exchange the values of two numbers using temporary variable?

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#1 Write a Python program to exchange the values of two numbers using temporary variable?
+
+
+a=10
+b=20
+print('before exchange')
+print(a,b)
+c=b
+b=a
+a=c
+print('after exchange')
+print(a,b)
