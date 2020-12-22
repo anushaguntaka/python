96612679d3dc7c48a4fc1652108c9c0c4fa51825
anushaguntaka <33531186+anushaguntaka@users.noreply.github.com>
2020-12-21 17:28:30 -0700

    Add files via upload

---
+++ Dynamic Even or Odd.py
@@ -0,0 +1,10 @@
+
+#5)Write a Python program to read the number and check whether the given number is Even or Odd number?
+
+num = input("enter a number:")
+
+if int(num)%2==0:
+    print("Given number is Even Number")
+else :
+    print("Given number is Odd Number")
+
