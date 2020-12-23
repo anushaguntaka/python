61e3475f0e97974b562b08d2b7e309ad62014cd7
anushaguntaka <33531186+anushaguntaka@users.noreply.github.com>
2020-12-23 14:47:22 -0700

    Add files via upload

---
+++ primenumber or not.py
@@ -0,0 +1,13 @@
+#8)Write a Python program to read the number and check whether the given number is a Prime number or not?
+
+num = input("enter a number:")
+
+if int(num)%2==0 :
+    print("it is not a prime number")
+elif int(num)%3==0:
+    print("it is not a prime number")
+else:
+    print("it is a prime number")
+
+
+
