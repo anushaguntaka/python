2a2b2f9533b9a7f5b61ee71e6199b1e023545bff
anushaguntaka <33531186+anushaguntaka@users.noreply.github.com>
2020-12-24 11:52:17 -0700

    Add files via upload

---
+++ min or max of numbers.py
@@ -0,0 +1,22 @@
+
+
+
+#11) write a python program to find the minimum of 3 numbers?
+
+
+'''num1 = int(input("enter 1st number:"))
+num2 = int(input("enter 2nd number:"))
+num3 = int(input("enter 3rd number:"))
+
+print(min(num1,num2,num3))'''
+
+
+#12) write a python program to find the maximum of 3 numbers?
+
+
+num1 = int(input("enter 1st number:"))
+num2 = int(input("enter 2nd number:"))
+num3 = int(input("enter 3rd number:"))
+
+print(max(num1,num2,num3))
+
