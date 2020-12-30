739dbad3282c8aef7e776b276800ffbe5bf25b9b
anushaguntaka <33531186+anushaguntaka@users.noreply.github.com>
2020-12-29 23:25:38 -0700

    Add files via upload

---
+++ stringprogrampart1.py
@@ -0,0 +1,37 @@
+#1)write a python program to reverse of a string with out using inbuilt functions?
+
+st='anu'
+print(st[::-1])
+print("________")
+
+#2)Write a python program to sort the string in ASC and DESC
+st='132'
+print(sorted(st))
+print("________")
+
+#3)How to declare the empty string?
+
+st=''
+print(st)
+print(type(st))
+print("________")
+
+#4)how to declare single character string?
+
+st="a"
+print(st)
+print(type(st))
+print("________")
+
+#5)How to convert a string object to list object?
+
+st='a'
+st1 = ['a']
+st2=list(st)
+st3=st.split()
+print(type(st1))
+print(type(st2))
+print(st3)
+print(type(st3))
+
+print("________")
