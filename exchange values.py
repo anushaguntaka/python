ff117325851da3c9fda69af7c302b8005f3de22d
anushaguntaka <33531186+anushaguntaka@users.noreply.github.com>
2021-01-08 23:01:41 -0700

    Add files via upload

---
+++ exchange values.py
@@ -0,0 +1,15 @@
+
+
+#exchange value of two numbers without using temporary variable
+
+var1 = eval(input("enter value of first variable"))
+var2 = eval(input("enter value of second variable"))
+
+var1 = var1 + var2
+
+var2 = var1 - var2
+
+var1 = var1 -var2
+
+print (var1)
+print(var2)
